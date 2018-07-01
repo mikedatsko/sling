6f77debe151233632c2d66eb49ecd9b26407b48c
Oleksandr Datsko <datsko.oleksandr@pandats.com>
2018-07-01 14:50:59 +0300

    change grafana dashboards

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 mkdir -p data
-docker build -t jmeter-base jmeter-base
+docker build -t jmeter-base jmeter-base grafana influxdb chronograf kapacitor
 docker-compose build && docker-compose up -d && docker-compose scale master=1 slave=$1
+
+
+echo "Wait 30 seconds while grafana starts"
 
 sleep 30
 
-# add grafana datasource and dashboards
+echo "add grafana datasource and dashboards"
 
 curl --user admin:admin -d @grafana/influxdb-source.json -H "Content-Type: application/json" -X POST "http://127.0.0.1:9001/api/datasources"
 curl --user admin:admin -d @grafana/jmeter-dashboard.json -H "Content-Type: application/json" -X POST "http://127.0.0.1:9001/api/dashboards/import"
-curl --user admin:admin -d @grafana/influxdb-metrics.json -H "Content-Type: application/json" -X POST "http://127.0.0.1:9001/api/dashboards/import"
+curl --user admin:admin -d @grafana/influxdb-1138.json -H "Content-Type: application/json" -X POST "http://127.0.0.1:9001/api/dashboards/import"
+curl --user admin:admin -d @grafana/influxdb-base-metrics.json -H "Content-Type: application/json" -X POST "http://127.0.0.1:9001/api/dashboards/import"
